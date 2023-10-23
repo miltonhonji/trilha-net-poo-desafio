ef1fa5d9bb7c279708ad3ad58072b576d5f52407
milton.honji <factorysolutionIt@outlook.com>
2023-10-23 16:55:13 -0300

    Exercício para entrega de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "551199663388", modelo: "Tijolão", imei: "1555222", memoria:32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Waze");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "551194451122", modelo: "Sonho de Consumo", imei: "12223", memoria:256);
+iphone.ReceberLigacao();
+nokia.InstalarAplicativo("Spotfy");
